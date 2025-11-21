1fddc90216e7c110edb6f8d84c9355067edf4e57
Tommy <tommylin19990127@gmail.com>
2025-11-21 15:25:13 +0800

    feat: 建立Post資料表連線

---
+++ Project_Photo/Migrations/Post/20251121072040_InitPost.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project_Photo.Migrations.Post
+{
+    /// <inheritdoc />
+    public partial class InitPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "SocialNetwork");
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                schema: "SocialNetwork",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    PostType = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    PostTypeId = table.Column<int>(type: "int", nullable: true),
+                    PostContent = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    ParentPostId = table.Column<int>(type: "int", nullable: true),
+                    GroupId = table.Column<int>(type: "int", nullable: true),
+                    EventId = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "(getdate())"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "(getdate())"),
+                    DeletedAt = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Posts",
+                        column: x => x.ParentPostId,
+                        principalSchema: "SocialNetwork",
+                        principalTable: "Posts",
+                        principalColumn: "PostId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_ParentPostId",
+                schema: "SocialNetwork",
+                table: "Posts",
+                column: "ParentPostId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts",
+                schema: "SocialNetwork");
+        }
+    }
+}
---
+++ Project_Photo/Partials/PostContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project_Photo.Models
+{
+    public partial class PostContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                                                      .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                                                      .AddJsonFile("appsettings.json")
+                                                      .Build();
+
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("AA"));
+            }
+        }
+    }
+}
