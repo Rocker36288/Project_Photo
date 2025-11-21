4dd2659a4c795aeeb7d43e6962370ea08b7adae3
icenightx <127119106+icenightx@users.noreply.github.com>
2025-11-21 10:25:34 +0800

    新增VideosControllers

---
+++ Project_Photo/Areas/Videos/Controllers/VideosController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Project_Photo.Areas.Videos.Models;
+
+namespace Project_Photo.Areas.Videos.Controllers
+{
+    [Area("Videos")]
+    public class VideosController : Controller
+    {
+        private readonly AaContext _context;
+
+        public VideosController(AaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Videos/Videos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Videos.ToListAsync());
+        }
+
+        // GET: Videos/Videos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var video = await _context.Videos
+                .FirstOrDefaultAsync(m => m.VideoId == id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            return View(video);
+        }
+
+        // GET: Videos/Videos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Videos/Videos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VideoId,ChannelId,Title,Description,VideoUrl,ThumbnailUrl,Duration,Resolution,FileSize,ProcessStatus,PrivacyStatus,CreatedAt,UpdateAt")] Video video)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(video);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(video);
+        }
+
+        // GET: Videos/Videos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var video = await _context.Videos.FindAsync(id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+            return View(video);
+        }
+
+        // POST: Videos/Videos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("VideoId,ChannelId,Title,Description,VideoUrl,ThumbnailUrl,Duration,Resolution,FileSize,ProcessStatus,PrivacyStatus,CreatedAt,UpdateAt")] Video video)
+        {
+            if (id != video.VideoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(video);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VideoExists(video.VideoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(video);
+        }
+
+        // GET: Videos/Videos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var video = await _context.Videos
+                .FirstOrDefaultAsync(m => m.VideoId == id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            return View(video);
+        }
+
+        // POST: Videos/Videos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var video = await _context.Videos.FindAsync(id);
+            if (video != null)
+            {
+                _context.Videos.Remove(video);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VideoExists(int id)
+        {
+            return _context.Videos.Any(e => e.VideoId == id);
+        }
+    }
+}
