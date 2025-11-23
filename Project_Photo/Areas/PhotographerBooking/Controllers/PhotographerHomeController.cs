881d309b4741583834c185f7b0aa1438e34749d3
blueblue999 <blueblue969696@gmail.com>
2025-11-24 06:17:12 +0800

    增加area--PhotographerBooking

---
+++ Project_Photo/Areas/PhotographerBooking/Controllers/PhotographerHomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Photo.Areas.PhotographerBooking.Controllers
+{
+    [Area("PhotographerBooking")]
+    public class PhotographerHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult OnSelve()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult OffSelve()
+        //{
+        //    return View();
+        //}
+    }
+}
