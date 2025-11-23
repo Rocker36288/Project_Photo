95553f6f25b9a667a1fe1a26b131e38478dd67b1
blueblue999 <blueblue969696@gmail.com>
2025-11-24 07:42:37 +0800

    加反向工程 Partials

---
+++ Project_Photo/Areas/PhotographerBooking/Partials/AAContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project_Photo.Areas.PhotographerBooking
+{
+    public partial class AAContext : DbContext
+    {
+
+
+        public AAContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfiguration Config = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                optionsBuilder.UseSqlServer(Config.GetConnectionString("AA"));
+            }
+        }
+
+    }
+
+}
