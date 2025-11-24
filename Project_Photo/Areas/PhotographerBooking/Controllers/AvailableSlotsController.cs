0fa11e861bae26b4c165beda0063bb399d544aee
blueblue969696@gmail.com <blueblue999>
2025-11-24 18:30:42 +0800

    新增Controllers

---
+++ Project_Photo/Areas/PhotographerBooking/Controllers/AvailableSlotsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Project_Photo.Models;
+
+namespace Project_Photo.Areas.PhotographerBooking.Controllers
+{
+    [Area("PhotographerBooking")]
+    public class AvailableSlotsController : Controller
+    {
+        private readonly AAContext _context;
+
+        public AvailableSlotsController(AAContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PhotographerBooking/AvailableSlots
+        public async Task<IActionResult> Index()
+        {
+            var aAContext = _context.AvailableSlots.Include(a => a.Photographer);
+            return View(await aAContext.ToListAsync());
+        }
+
+        // GET: PhotographerBooking/AvailableSlots/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var availableSlot = await _context.AvailableSlots
+                .Include(a => a.Photographer)
+                .FirstOrDefaultAsync(m => m.AvailableSlotId == id);
+            if (availableSlot == null)
+            {
+                return NotFound();
+            }
+
+            return View(availableSlot);
+        }
+
+        // GET: PhotographerBooking/AvailableSlots/Create
+        public IActionResult Create()
+        {
+            ViewData["PhotographerId"] = new SelectList(_context.Photographers, "PhotographerId", "StudioName");
+            return View();
+        }
+
+        // POST: PhotographerBooking/AvailableSlots/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AvailableSlotId,PhotographerId,SlotDate,StartTime,EndTime,BookingId,CreatedAt")] AvailableSlot availableSlot)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(availableSlot);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PhotographerId"] = new SelectList(_context.Photographers, "PhotographerId", "StudioName", availableSlot.PhotographerId);
+            return View(availableSlot);
+        }
+
+        // GET: PhotographerBooking/AvailableSlots/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var availableSlot = await _context.AvailableSlots.FindAsync(id);
+            if (availableSlot == null)
+            {
+                return NotFound();
+            }
+            ViewData["PhotographerId"] = new SelectList(_context.Photographers, "PhotographerId", "StudioName", availableSlot.PhotographerId);
+            return View(availableSlot);
+        }
+
+        // POST: PhotographerBooking/AvailableSlots/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AvailableSlotId,PhotographerId,SlotDate,StartTime,EndTime,BookingId,CreatedAt")] AvailableSlot availableSlot)
+        {
+            if (id != availableSlot.AvailableSlotId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(availableSlot);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AvailableSlotExists(availableSlot.AvailableSlotId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PhotographerId"] = new SelectList(_context.Photographers, "PhotographerId", "StudioName", availableSlot.PhotographerId);
+            return View(availableSlot);
+        }
+
+        // GET: PhotographerBooking/AvailableSlots/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var availableSlot = await _context.AvailableSlots
+                .Include(a => a.Photographer)
+                .FirstOrDefaultAsync(m => m.AvailableSlotId == id);
+            if (availableSlot == null)
+            {
+                return NotFound();
+            }
+
+            return View(availableSlot);
+        }
+
+        // POST: PhotographerBooking/AvailableSlots/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var availableSlot = await _context.AvailableSlots.FindAsync(id);
+            if (availableSlot != null)
+            {
+                _context.AvailableSlots.Remove(availableSlot);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AvailableSlotExists(int id)
+        {
+            return _context.AvailableSlots.Any(e => e.AvailableSlotId == id);
+        }
+    }
+}
---
+++ Project_Photo/Areas/PhotographerBooking/Controllers/SpecialtyTagsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Project_Photo.Models;
+
+namespace Project_Photo.Areas.PhotographerBooking.Controllers
+{
+    [Area("PhotographerBooking")]
+    public class SpecialtyTagsController : Controller
+    {
+        private readonly AAContext _context;
+
+        public SpecialtyTagsController(AAContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PhotographerBooking/SpecialtyTags
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.SpecialtyTags.ToListAsync());
+        }
+
+        // GET: PhotographerBooking/SpecialtyTags/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var specialtyTag = await _context.SpecialtyTags
+                .FirstOrDefaultAsync(m => m.SpecialtyTagId == id);
+            if (specialtyTag == null)
+            {
+                return NotFound();
+            }
+
+            return View(specialtyTag);
+        }
+
+        // GET: PhotographerBooking/SpecialtyTags/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PhotographerBooking/SpecialtyTags/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SpecialtyTagId,SpecialtyName,Category,Description,DisplayOrder,IsActive,CreatedAt")] SpecialtyTag specialtyTag)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(specialtyTag);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(specialtyTag);
+        }
+
+        // GET: PhotographerBooking/SpecialtyTags/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var specialtyTag = await _context.SpecialtyTags.FindAsync(id);
+            if (specialtyTag == null)
+            {
+                return NotFound();
+            }
+            return View(specialtyTag);
+        }
+
+        // POST: PhotographerBooking/SpecialtyTags/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SpecialtyTagId,SpecialtyName,Category,Description,DisplayOrder,IsActive,CreatedAt")] SpecialtyTag specialtyTag)
+        {
+            if (id != specialtyTag.SpecialtyTagId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(specialtyTag);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SpecialtyTagExists(specialtyTag.SpecialtyTagId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(specialtyTag);
+        }
+
+        // GET: PhotographerBooking/SpecialtyTags/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var specialtyTag = await _context.SpecialtyTags
+                .FirstOrDefaultAsync(m => m.SpecialtyTagId == id);
+            if (specialtyTag == null)
+            {
+                return NotFound();
+            }
+
+            return View(specialtyTag);
+        }
+
+        // POST: PhotographerBooking/SpecialtyTags/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var specialtyTag = await _context.SpecialtyTags.FindAsync(id);
+            if (specialtyTag != null)
+            {
+                _context.SpecialtyTags.Remove(specialtyTag);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SpecialtyTagExists(int id)
+        {
+            return _context.SpecialtyTags.Any(e => e.SpecialtyTagId == id);
+        }
+    }
+}
